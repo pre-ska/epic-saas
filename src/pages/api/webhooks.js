5916b5c6721964aeb7329af13315d0347232b503
preska <wolfwar@outlook.com>
2023-09-20 14:22:34 +0200

    stripe webhook user update

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,1 +1,27 @@
-export default async function handler(req, res) {}
+import getRawBody from "raw-body";
+import { stripe } from "src/pricing/utils/stripe";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const signature = req.headers["stripe-signature"];
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+
+  let event;
+
+  try {
+    const rawBody = await getRawBody(req, { limit: "2mb" });
+    event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
+  } catch (error) {
+    console.log("Webhook signature verification failed");
+    return res.status(400).end();
+  }
+
+  console.log("EVENT WEBHOOK", event);
+
+  res.send({ success: true });
+}
